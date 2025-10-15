c472930599a4444df7019ef7df5792bcd29dc5aa
Nguyen Tri <nneenterpriseinc@gmail.com>
2025-10-15 16:35:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5302783866-e8cbe",
+  "appId": "1:712849024007:web:227b92c686fc3007f3f6a2",
+  "apiKey": "AIzaSyDqAy8d-b9fL8Sau8pmA-H6IKABDbh1QAU",
+  "authDomain": "studio-5302783866-e8cbe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "712849024007"
+};
